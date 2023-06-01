a306c9e8574dc8b0e352a27c91cc905d99db2e8b
Tharun <tharun.vedts@gmail.com>
2023-06-01 10:37:26 +0530

    commit by mallikarjun

--- ems/src/main/java/com/vts/ems/newspaper/dao/NewsPaperDao.java
+++ ems/src/main/java/com/vts/ems/newspaper/dao/NewsPaperDao.java
@@ -97,9 +97,9 @@ public interface NewsPaperDao {
 	public List<Object[]> getTelephoneUserPrintMultiData(String TeleForwardId);
 	public List<Object[]> getTelephoneUserPrintSingleData(String TeleForwardId);
 	public List<Object[]> getTelephoneUserPrintSingleDataByMonth(String TeleForwardId,String teleId);//me added
-	public List<Object[]> newsPaperRemarksHistory(String NewspaperId) throws Exception;
+	public List<Object[]> newsPaperRemarksHistory(String NewspaperId) throws Exception; 
 	
-	
+	 
 	
 
 }
